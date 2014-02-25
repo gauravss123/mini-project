7f0146f40687a8a7ec71c72196c310f1acbf8854
Gaurav <gauravfos@gmail.com>
2014-02-25 23:19:30 +0530

    Adding comment and completed till calculating mg

--- mini.m
+++ mini.m
@@ -1,15 +1,16 @@
-I = imread('/media/gaurav/LocAL/SAMPLE/lena.jpg');
-%imshow(I);
-G= (.222*I(:,:,1))+(.707*I(:,:,2))+(.071*I(:,:,3));
+I = imread('/media/gaurav/LocAL/SAMPLE/luna.jpg');
 
-%imshow(G);
+imshow(I);
+G= (.222*I(:,:,1))+(.707*I(:,:,2))+(.071*I(:,:,3));
+H=G;
+figure;
+imshow(H);
 g=G(:)';
 y = zeros(1,256);
 t=size(G);
 max=t(1)*t(2);
 for i=1:max
-    y(g(i))=y(g(i))+1;
+   y(g(i)+1)=y(g(i)+1)+1;
 end;
-stem(y);
 figure;
-imhist(G);
+stem(y);
--- mini2.m
+++ mini2.m
@@ -1,24 +1,31 @@
-I = imread('/media/gaurav/LocAL/SAMPLE/lena.jpg');
+I = imread('/media/gaurav/LocAL/SAMPLE/luna.jpg');
+%imshow(I);
 
-imshow(I);
-G= (.222*I(:,:,1))+(.707*I(:,:,2))+(.071*I(:,:,3));
-H=G;
+G= (.222*I(:,:,1))+(.707*I(:,:,2))+(.071*I(:,:,3)); %convert to grayscale, adjust to human eye sensitivity
+figure;
+imshow(G);
 
-imshow(H);
-g=G(:)';
-y = zeros(1,256);
-w = zeros(1,256);
+g=G(:)'; %convert 2d grayscale matrix to 1d
+y = zeros(1,256); %1d array to store values of histogram
+w = zeros(1,256); %1d array to store values of normalised histogram
 
-t=size(G);
-max=t(1)*t(2);
+t=size(G); % returns dimension of grayscale image
+max=t(1)*t(2); % no. of pixels in image
 for i=1:max
-   y(g(i))=y(g(i))+1;
+   y(g(i)+1)=y(g(i)+1)+1; % calculate histogram, +1 on lhs to handle case when intensity value =0, rhs calculate frequency
 end;
 for i=1:max
-   w=y/t;
+   w=y/max; % to calculate normalised histogram
 end;
+%for i=1:max
+   %wg(g(i)+1)=wg(g(i)+1)+1;
+%end;
+wg = 0;
+for i=1:256
+    wg=(i*w(i)); % calculate mg
+end;
+%subplot(1,2,1)
 
-subplot(1,2,1)
-stem(y);
-subplot(1,2,2)
-stem(w);
+%stem(y);
+%subplot(1,2,2)
+%stem(w);
