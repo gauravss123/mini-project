14026aa8c8cfe0df05dfbae216cb6010131d2efa
Gaurav <gauravfos@gmail.com>
2014-02-25 23:29:08 +0530

    Further addition

--- mini2.m
+++ mini2.m
@@ -24,8 +24,25 @@ wg = 0;
 for i=1:256
     wg=(i*w(i)); % calculate mg
 end;
+k=0;
+temp=0;
+p1=0;
+m=0;
+for ke=0:255
+    for l= 1:(ke+1) %calculate values of P1 and m
+        p1=p1+w(i);
+        m=m+(i*w(i));
+    end
+    temp = %calculate variance at k
+    %ToDo compare to replace maximum value of variance
+end
+
+
 %subplot(1,2,1)
 
 %stem(y);
 %subplot(1,2,2)
 %stem(w);
+
+%ToDo make zero for all values in G less than equal to 'k' and 1 greater than k
+
